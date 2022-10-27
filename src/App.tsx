a3fe0be73362ffee15a7b4cb436a5a914443b5f7
Dennisss <denzeldenisq@gmail.com>
2022-10-27 21:14:48 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="h-screen w-full flex justify-center">
+
+    </div>
+  )
+}
+
+export default App
